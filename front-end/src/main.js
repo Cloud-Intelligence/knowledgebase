d26e7a39f0030e50ddddd67af1046f14849514a2
jimmy b <mail@jimmyb.co.za>
2021-07-20 08:24:29 +0200

    added auth using 0auth

--- front-end/src/main.js
+++ front-end/src/main.js
@@ -4,6 +4,25 @@ import router from './router'
 
 import 'bulma/css/bulma.css';
 
+// Import the Auth0 configuration
+import { domain, clientId } from "../auth_config.json";
+
+// Import the plugin here
+import { Auth0Plugin } from "./auth";
+
+// Install the authentication plugin here
+Vue.use(Auth0Plugin, {
+  domain,
+  clientId,
+  onRedirectCallback: appState => {
+    router.push(
+      appState && appState.targetUrl
+        ? appState.targetUrl
+        : window.location.pathname
+    );
+  }
+});
+
 Vue.config.productionTip = false
 
 new Vue({
--- front-end/src/views/Home.vue
+++ front-end/src/views/Home.vue
@@ -1,14 +1,28 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
+  <div class="home" id="home">
+    <header>
+      <navbar></navbar>
+    </header>
+    <section class="body" id="body">
+      <p>Name: {{$auth.user.given_name}}</p>
+      <p>Surname: {{$auth.user.family_name}}</p>
+      <p>email: {{$auth.user.email}}</p>
+      <p>verified: {{$auth.user.email_verified}}</p>
+    </section>
   </div>
 </template>
 
 <script>
+import Navbar from '../components/Navbar.vue';
 
 export default {
-  name: 'Home',
+  name: 'home',
   components: {
-  },
-};
+    Navbar
+  }
+}
+
 </script>
+
+<style lang="scss" scoped>
+</style>
