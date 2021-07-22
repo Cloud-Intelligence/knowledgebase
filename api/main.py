3509fdbf3caf2da4bb4a7066ec94d4d06bbd2d67
jimmy b <mail@jimmyb.co.za>
2021-07-22 09:33:52 +0200

    start using db for files

--- api/main.py
+++ api/main.py
@@ -4,7 +4,7 @@ from werkzeug.wrappers import response
 from loguru import logger
 
 from authentication import AuthError, requires_auth, requires_scope, get_token_auth_header
-from utils.file_tools import file_list, load_file
+from utils.file_tools import file_list, load_file, save_file
 
 APP = Flask(__name__)
 CORS(APP)
@@ -17,19 +17,38 @@ def handle_auth_error(ex):
     return response
 
 
-@APP.route("/api/public")
+@APP.route("/api/health")
 @cross_origin(headers=["Content-Type", "Authorization"])
 def public():
-    response = "Public Endpoint"
+    response = "Healthy"
     return jsonify(message=response)
 
 
-@APP.route("/api/private/")
+@APP.route('/api/documents/', methods=['GET', 'POST'])
 @cross_origin(headers=["Content-Type", "Authorization"])
 @requires_auth
-def private():
-    response = "Private endpoint"
-    return jsonify(message=response)
+def index():
+    if request.method == "POST":
+        save_file("test", "test", '')
+        return jsonify(success=True)
+    else:
+        files = file_list(request.current_user)
+        return jsonify(data=files)
+
+
+@APP.route("/api/documents/<uuid:id>")
+@cross_origin(headers=["Content-Type", "Authorization"])
+@requires_auth
+def detail(filepath):
+    try:
+        load_file(filepath)
+    except FileNotFoundError:
+        raise AuthError({
+            "code": "File not found",
+            "description": "The file you have requested does not exist"
+        }, 404)
+
+    return jsonify(message='TDB')
 
 
 @APP.route("/api/private-scoped")
@@ -46,32 +65,6 @@ def private_scoped():
         },
         403,
     )
-
-
-@APP.route('/')
-@cross_origin(headers=["Content-Type", "Authorization"])
-@requires_auth
-def index():
-    logger.info(request.current_user)
-    files = file_list()
-    response = jsonify(message=dir(request))
-    return response
-
-
-@APP.route("/md/<string:filepath>")
-@cross_origin(headers=["Content-Type", "Authorization"])
-@requires_auth
-def detail(filepath):
-    try:
-        load_file(filepath)
-    except FileNotFoundError:
-        raise AuthError({
-            "code": "File not found",
-            "description": "The file you have requested does not exist"
-        }, 404)
-
-    return jsonify(message='TDB')
-
 
 if __name__ == '__main__':
     APP.run(debug=True, host='0.0.0.0')
