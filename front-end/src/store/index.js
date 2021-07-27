cb8a698d6e86a874019b7bfb51ecd787887188a4
Sebastian-Paterson-Jones <sebastian.boo.pj@gmail.com>
2021-07-27 10:29:12 +0200

    finished frame for home page as a reference for other pages until backend implemented

---
+++ front-end/src/store/index.js
@@ -0,0 +1,25 @@
+import Vue from 'vue';
+import Vuex from 'vuex';
+
+Vue.use(Vuex);
+
+export default new Vuex.Store({
+  state: {
+    contentView: {
+      title: 'Home',
+      topic: 'Home',
+      content: '## this is home',
+      tags: ['#cloudIntelligence', '#home', '#booyah'],
+    },
+  },
+  getters: {},
+  mutations: {
+    setContentView(state, title, topic, content, tags) {
+      // eslint-disable-next-line no-param-reassign
+      state.contentView = {
+        title, topic, content, tags,
+      };
+    },
+  },
+  actions: {},
+});
