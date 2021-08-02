ccd9f73888e54530b498a0eab953daddf6021a43
Sebastian-Paterson-Jones <sebastian.boo.pj@gmail.com>
2021-08-02 11:55:31 +0200

    sidebar and content view cypress tests

--- front-end/cypress/integration/sidebar.spec.js
+++ front-end/cypress/integration/sidebar.spec.js
@@ -1,6 +1,7 @@
 describe('sidebar', () => {
   it('matches image baseline', () => {
     cy.visit('http://localhost:8080');
+    cy.wait(500);
     cy.get('#sidebar').toMatchImageSnapshot();
   });
   it('is toggleable', () => {
@@ -19,11 +20,37 @@ describe('sidebar', () => {
       expect(userName.length).to.be.greaterThan(0);
     });
   });
+  it('styles sidebar link on click', () => {
+    cy.visit('http://localhost:8080');
+    cy.get('#Favorites').click();
+    cy.get('#sidebar').toMatchImageSnapshot();
+  });
   it('contains defualt links', () => {
     cy.visit('http://localhost:8080');
     cy.contains('Home');
     cy.contains('Search');
     cy.contains('Favorites');
+  });
+  it('routes to Home page on click', () => {
+    cy.visit('http://localhost:8080');
+    cy.get('#Home').click();
+    cy.get('.body').toMatchImageSnapshot();
+  });
+  it('routes to Search page on click', () => {
+    cy.visit('http://localhost:8080');
+    cy.get('#Search').click();
+    cy.get('.body').toMatchImageSnapshot();
+  });
+  it('routes to favorites page on click', () => {
+    cy.visit('http://localhost:8080');
+    cy.get('#Favorites').click();
+    cy.get('.body').toMatchImageSnapshot();
+  });
+  it('routes to doc page on click of document', () => {
+    cy.visit('http://localhost:8080');
+    cy.get('.topic button').first().click();
+    cy.get('#12').click();
+    cy.get('.body').toMatchImageSnapshot();
   });
   it('contains dropdown collections', () => {
     cy.visit('http://localhost:8080');
@@ -32,7 +59,7 @@ describe('sidebar', () => {
   it('has toggleable dropdown collection', () => {
     cy.visit('http://localhost:8080');
     cy.get('.topic button').first().click();
-    cy.get('.topic .children').should('have.css', 'max-height');
+    cy.get('.topic .children').first().toMatchImageSnapshot();
   });
   it('has children under collections', () => {
     cy.visit('http://localhost:8080');
