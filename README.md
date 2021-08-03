19daa0e624d89e776c89561cd51a454efe656a65
James Burgess <mail@jimmyb.co.za>
2021-08-03 14:11:34 +0200

    Cypress CI Test (#17)
    * CI / CD for UI
    
    Run tests and static analysis on files
    
    * Static code analysis for js files
    
    eslint, jshit, goodcheck and misspell
    
    * use working dir
    
    * test
    
    * new tools
    
    * Update static.yml
    
    * Update static.yml
    
    integrated mock-api and backend api to intrepolate depending on environment
    
    referenced env files and fixed cypress tests
    
    Cypress CI Test
    
    turn off websecurity on test
    
    run cypress headlessly in container
    
    change workdir
    
    new workdir syntax
    
    rename test
    
    update test screens
    
    ci
    
    ci
    
    ci
    
    ci
    
    ci
    
    * updated cypress to run on matching browsers
    
    * update README

--- README.md
+++ README.md
@@ -1,16 +1,12 @@
 # knowledgebase Wiki
+[![E2E Test](https://github.com/Cloud-Intelligence/knowledgebase/actions/workflows/main.yml/badge.svg)](https://github.com/Cloud-Intelligence/knowledgebase/actions/workflows/main.yml)
 
 Stuff for the mind
 
 # Running
 You will need docker and docker-compose set up in order to run this.
-
-- run `task` to start up the project
-- alternativley run `docker-compose up` if you do not have taskfile installed
-- head over to localhost:3000/
-- use your corpo email to login
-- default password is `gregcantcode`
-- learn
-
-# Adding content
-Content is written in [markdown](https://www.markdownguide.org/basic-syntax/) and is saved automaticcaly to git, just push when you are done.
+- install [taskfile](https://taskfile.dev/)
+- install [docker-compose](https://docs.docker.com/compose/install/)
+- run `task install` to install deps
+- run `task` to start the apps
+- checkout the taskfile for other scripts
--- front-end/cypress/integration/sidebar.spec.js
+++ front-end/cypress/integration/sidebar.spec.js
@@ -1,16 +1,16 @@
 describe('sidebar', () => {
   it('matches image baseline', () => {
-    cy.visit('http://localhost:8080');
+    cy.visit('/');
     cy.wait(500);
     cy.get('#sidebar').toMatchImageSnapshot();
   });
   it('is toggleable', () => {
-    cy.visit('http://localhost:8080');
+    cy.visit('/');
     cy.get('.collapse_toggle').click();
     cy.get('#sidebar').toMatchImageSnapshot();
   });
   it('contains user details', () => {
-    cy.visit('http://localhost:8080');
+    cy.visit('/');
     cy.get('.user_description .title').then((title) => {
       const descTitle = title.text();
       expect(descTitle).to.equal('Cloud intelligence');
@@ -21,48 +21,48 @@ describe('sidebar', () => {
     });
   });
   it('styles sidebar link on click', () => {
-    cy.visit('http://localhost:8080');
+    cy.visit('/');
     cy.get('#Favorites').click();
     cy.get('#sidebar').toMatchImageSnapshot();
   });
   it('contains defualt links', () => {
-    cy.visit('http://localhost:8080');
+    cy.visit('/');
     cy.contains('Home');
     cy.contains('Search');
     cy.contains('Favorites');
   });
   it('routes to Home page on click', () => {
-    cy.visit('http://localhost:8080');
+    cy.visit('/');
     cy.get('#Home').click();
     cy.get('.body').toMatchImageSnapshot();
   });
   it('routes to Search page on click', () => {
-    cy.visit('http://localhost:8080');
+    cy.visit('/');
     cy.get('#Search').click();
     cy.get('.body').toMatchImageSnapshot();
   });
   it('routes to favorites page on click', () => {
-    cy.visit('http://localhost:8080');
+    cy.visit('/');
     cy.get('#Favorites').click();
     cy.get('.body').toMatchImageSnapshot();
   });
   it('routes to doc page on click of document', () => {
-    cy.visit('http://localhost:8080');
+    cy.visit('/');
     cy.get('.topic button').first().click();
     cy.get('#12').click();
     cy.get('.body').toMatchImageSnapshot();
   });
   it('contains dropdown collections', () => {
-    cy.visit('http://localhost:8080');
+    cy.visit('/');
     cy.get('.topic');
   });
   it('has toggleable dropdown collection', () => {
-    cy.visit('http://localhost:8080');
+    cy.visit('/');
     cy.get('.topic button').first().click();
     cy.get('.topic .children').first().toMatchImageSnapshot();
   });
   it('has children under collections', () => {
-    cy.visit('http://localhost:8080');
+    cy.visit('/');
     cy.get('.children.collapsed').children().should('exist');
   });
 });
