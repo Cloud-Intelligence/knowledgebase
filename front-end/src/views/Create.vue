7e9c34851889dafa0e5e85f071a5f292c40dd7bf
Sebastian-Paterson-Jones <sebastian.boo.pj@gmail.com>
2021-08-04 09:30:49 +0200

    added new create page and removed redundant data in documents view

--- front-end/src/views/Create.vue
+++ front-end/src/views/Create.vue
@@ -2,6 +2,7 @@
   <section class="main" id="main">
     <div class="container">
       <div class="layout">
+<<<<<<< HEAD
         <div class="form columns">
           <div class="fields column">
             <div class="dropdown" id="topics">
@@ -187,12 +188,80 @@
         >
           This field is required
         </p>
+=======
+        <div class="fields">
+          <div class="dropdown is-active">
+            <div class="dropdown-trigger">
+              <button
+                class="button"
+                aria-haspopup="true"
+                aria-controls="dropdown-menu"
+              >
+                <span>{{topic}}</span>
+                <span class="icon is-small">
+                  <i class="fas fa-angle-down" aria-hidden="true"></i>
+                </span>
+              </button>
+            </div>
+            <div class="dropdown-menu" id="dropdown-menu" role="menu">
+              <div class="dropdown-content">
+                <a
+                  v-for="(topic, index) in loaded_topics"
+                  class="dropdown-item"
+                  :key="index"
+                >
+                  {{ topic }}
+                </a>
+                <hr class="dropdown-divider" />
+                <div class="add_topic columns">
+                  <input class="input column is-three-quarters" type="text" placeholder="#Topic" />
+                  <button class="button column add_button">+</button>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <input class="input" type="text" placeholder="#Title" />
+
+          <div class="dropdown is-active">
+            <div class="dropdown-trigger">
+              <button
+                class="button"
+                aria-haspopup="true"
+                aria-controls="dropdown-menu"
+              >
+                <span v-for="(tag, index) in tags" class="tag " :key="index">{{tag}}</span>
+                <span class="icon is-small">
+                  <i class="fas fa-angle-down" aria-hidden="true"></i>
+                </span>
+              </button>
+            </div>
+            <div class="dropdown-menu" id="dropdown-menu" role="menu">
+              <div class="dropdown-content">
+                <a
+                  v-for="(tag, index) in loaded_tags"
+                  class="dropdown-item"
+                  :key="index"
+                >
+                  {{ tag }}
+                </a>
+                <hr class="dropdown-divider" />
+                <div class="add_topic columns">
+                  <input class="input column is-three-quarters" type="text" placeholder="#Tag" />
+                  <button class="button column add_button">+</button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+>>>>>>> c63005a (added new create page and removed redundant data in documents view)
       </div>
     </div>
   </section>
 </template>
 
 <script>
+<<<<<<< HEAD
 import {
   UilAngleDown,
   UilCheck,
@@ -209,8 +278,13 @@ import 'quill/dist/quill.snow.css';
 // eslint-disable-next-line import/no-extraneous-dependencies
 import 'quill/dist/quill.bubble.css';
 
+<<<<<<< HEAD
 import { listTags, listTopics, postDocument } from '../api/documents';
 
+=======
+=======
+>>>>>>> c63005a (added new create page and removed redundant data in documents view)
+>>>>>>> 6f1512a (added new create page and removed redundant data in documents view)
 export default {
   data() {
     return {
@@ -220,6 +294,7 @@ export default {
       tags: [],
       loaded_topics: [],
       loaded_tags: [],
+<<<<<<< HEAD
       new_topic: '',
       new_tag: '',
       is_valid: true,
@@ -305,3 +380,36 @@ export default {
 <style lang="scss">
 @import "../assets/create-view.scss";
 </style>
+=======
+    };
+  },
+  created() {
+    fetch(`${process.env.VUE_APP_BASE_API_URL}/api/documents`)
+      .then((res) => res.json())
+      .then((json) => {
+        const files = json.data;
+        const tmpTopics = [];
+        const tmpTags = [];
+        // eslint-disable-next-line no-plusplus
+        for (let i = 0; i < files.length; i++) {
+          const fileTopic = files[i].topic;
+          const fileTags = files[i].tags;
+          if (!(fileTopic in tmpTopics)) {
+            tmpTopics.push(fileTopic);
+          }
+          // eslint-disable-next-line no-plusplus
+          for (let j = 0; j < fileTags.length; j++) {
+            const tag = fileTags[j];
+            if (!(tag in tmpTags)) {
+              tmpTags.push(tag);
+            }
+          }
+        }
+        this.topics = tmpTopics;
+        this.tags = tmpTags;
+        console.log(this.topics);
+      });
+  },
+};
+</script>
+>>>>>>> c63005a (added new create page and removed redundant data in documents view)
