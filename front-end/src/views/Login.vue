c77d0b3260a3446740cb9c6f89db77ddddaa5f61
Sebastian-Paterson-Jones <sebastian.boo.pj@gmail.com>
2021-08-20 08:06:25 +0200

    created frame for login page

--- front-end/src/views/Login.vue
+++ front-end/src/views/Login.vue
@@ -1,7 +1,20 @@
 <template>
-    <router-link to="/">
-    login
-    </router-link>
+  <section class="main" id="main">
+    <div class="container">
+      <div class="layout">
+        <div class="login">
+          <div class="box">
+            <div class="image">
+
+            </div>
+          <router-link to="/">
+            login
+          </router-link>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
@@ -9,3 +22,13 @@ export default {
   name: 'login',
 };
 </script>
+
+<style>
+#main .login {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+</style>
