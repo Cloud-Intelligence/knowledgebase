845ded40becd5a2de85bc4facbb1965760d2b39a
Sebastian-Paterson-Jones <sebastian.boo.pj@gmail.com>
2021-09-15 19:36:30 +0200

    test updated

--- front-end/cypress/integration/edit.spec.js
+++ front-end/cypress/integration/edit.spec.js
@@ -7,36 +7,16 @@ describe('edit', () => {
         cy.wait(500);
         cy.get('#main').toMatchImageSnapshot();
     });
-    it('has form fields', () => {
+    it('From fields are populated', () => {
         cy.contains('#topic');
         cy.contains('#tags');
         cy.contains('save');
     });
-    it('has topics dropdown', () => {
+    it('has topics in dropdown', () => {
         cy.get('.dropdown-trigger').first().click();
         cy.get('.dropdown-trigger').first().toMatchImageSnapshot();
         cy.get('.dropdown-menu .dropdown-content .add_element').first().toMatchImageSnapshot();
         cy.get('.dropdown-trigger').first().click();
-    });
-    it('validates user input', () => {
-        cy.get('.save.button').click();
-        cy.get('#main').toMatchImageSnapshot();
-    });
-    it('corrects validation on field input', () => {
-        cy.get('#title').type('Test');
-
-        cy.get('#topics .dropdown-trigger').click();
-        cy.get('.dropdown-item.add_element input').first().type('New topic');
-        cy.get('.dropdown-item.add_element button').first().click();
-        cy.get('#topics .dropdown-trigger').click();
-
-        cy.get('#tags .dropdown-trigger').click();
-        cy.get('.dropdown-item.add_element input').last().type('New tag');
-        cy.get('.dropdown-item.add_element button').last().click();
-        cy.get('#tags .dropdown-trigger').click();
-
-        cy.get('.ql-editor').type('test');
-        cy.get('#main').toMatchImageSnapshot();
     });
     it('redirects after the document has been updated', () => {
         cy.get('.save.button').click();
