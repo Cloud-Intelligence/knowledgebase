4d92dfe1374cbcd26463e7018b59ad424cc06e98
Sebastian-Paterson-Jones <sebastian.boo.pj@gmail.com>
2021-09-15 20:05:02 +0200

    test finally fixed and scrots updated

--- front-end/cypress/integration/edit.spec.js
+++ front-end/cypress/integration/edit.spec.js
@@ -2,15 +2,14 @@ describe('edit', () => {
     it('routes and matches image baseline of edit doc', () => {
         cy.visit('/');
         cy.get('.topic button').first().click();
-        cy.get('#12').click();
-        cy.get('.edit').click();
+        cy.get('#12').first().click();
+        cy.get('.heading .container.icons .edit').first().click();
         cy.wait(500);
         cy.get('#main').toMatchImageSnapshot();
     });
     it('From fields are populated', () => {
-        cy.contains('#topic');
-        cy.contains('#tags');
-        cy.contains('save');
+        cy.get('.fields.column').toMatchImageSnapshot();
+        cy.get('#editor').toMatchImageSnapshot();
     });
     it('has topics in dropdown', () => {
         cy.get('.dropdown-trigger').first().click();
