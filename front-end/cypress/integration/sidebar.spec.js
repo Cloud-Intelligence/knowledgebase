e936afa45c50130913144f62744e96492c2eed0e
Sebastian-Paterson-Jones <sebastian.boo.pj@gmail.com>
2021-09-16 09:30:02 +0200

    waits have been applied to cypress tests to give time delay for prod testing

--- front-end/cypress/integration/sidebar.spec.js
+++ front-end/cypress/integration/sidebar.spec.js
@@ -7,6 +7,7 @@ describe('sidebar', () => {
   it('is toggleable', () => {
     cy.visit('/');
     cy.get('.collapse_toggle').click();
+    cy.wait(50);
     cy.get('#sidebar').toMatchImageSnapshot();
   });
   it('contains user details', () => {
@@ -23,6 +24,7 @@ describe('sidebar', () => {
   it('styles sidebar link on click', () => {
     cy.visit('/');
     cy.get('#Favorites').click();
+    cy.wait(50);
     cy.get('#sidebar').toMatchImageSnapshot();
   });
   it('contains defualt links', () => {
@@ -34,27 +36,32 @@ describe('sidebar', () => {
   it('routes to Home page on click', () => {
     cy.visit('/');
     cy.get('#Home').click();
+    cy.wait(50);
     cy.get('.body').toMatchImageSnapshot();
   });
   it('routes to Search page on click', () => {
     cy.visit('/');
     cy.get('#Search').click();
+    cy.wait(50);
     cy.get('.body').toMatchImageSnapshot();
   });
   it('routes to favorites page on click', () => {
     cy.visit('/');
     cy.get('#Favorites').click();
+    cy.wait(50);
     cy.get('.body').toMatchImageSnapshot();
   });
   it('routes to doc page on click of document', () => {
     cy.visit('/');
     cy.get('.topic button').first().click();
     cy.get('#12').click();
+    cy.wait(50);
     cy.get('.body').toMatchImageSnapshot();
   });
   it('routes to create page on click of plus sign', () => {
     cy.visit('/');
     cy.get('a.add_record').click();
+    cy.wait(50);
     cy.get('.body').toMatchImageSnapshot();
   });
   it('contains dropdown collections', () => {
@@ -64,6 +71,7 @@ describe('sidebar', () => {
   it('has toggleable dropdown collection', () => {
     cy.visit('/');
     cy.get('.topic button').first().click();
+    cy.wait(50);
     cy.get('.topic .children').first().toMatchImageSnapshot();
   });
   it('has children under collections', () => {
