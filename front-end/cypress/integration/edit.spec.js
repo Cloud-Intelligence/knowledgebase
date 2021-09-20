e2aae728245db1819d14528c80c1783b88a64f9b
Sebastian-Paterson-Jones <sebastian.boo.pj@gmail.com>
2021-09-20 09:30:23 +0200

    scrots and test updated

--- front-end/cypress/integration/edit.spec.js
+++ front-end/cypress/integration/edit.spec.js
@@ -5,7 +5,7 @@ describe('edit', () => {
         cy.get('#12').first().click();
         cy.get('.heading .container.icons .edit').first().click();
         cy.wait(500);
-        cy.get('#main').toMatchImageSnapshot();
+        cy.get('#Edit').toMatchImageSnapshot();
     });
     it('From fields are populated', () => {
         cy.get('.fields.column').toMatchImageSnapshot();
