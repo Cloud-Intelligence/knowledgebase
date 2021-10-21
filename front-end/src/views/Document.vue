c05059d7bc1de5f505ae3212f5d363523b592d14
Jazza <jareddavidwild4@gmail.com>
2021-10-21 15:08:01 +0200

    Dynamic colour tags (#62)
    * Dynamic colour tags
    
    * update scrots
    new colored tags in document view require updated scrots
    
    * update scrots
    new scrots to match rebase from master and color tag branch
    
    * create some helper scripts for dealing with scrots
    
    * run cypress in parrellel in github
    
    * fix spelling in taskfile
    
    Co-authored-by: jimmy b <mail@jimmyb.co.za>

--- front-end/src/views/Document.vue
+++ front-end/src/views/Document.vue
@@ -38,7 +38,7 @@
     </div>
     <div class="foot">
       <div class="container tags">
-        <p v-for="tag in tags" class="tag" :key="tag">{{ tag }}</p>
+        <p v-for="tag in tags" class="tag" :key="tag" :style="getBackgroundColor(tag)">{{ tag }}</p>
       </div>
     </div>
   </section>
@@ -97,6 +97,32 @@ export default {
     },
     editDoc() {
       this.$router.push({ path: `/edit/${this.id}` });
+    },
+    getBackgroundColor(tag) {
+      const opts = {};
+      opts.hue = opts.hue || [0, 360];
+      opts.sat = opts.sat || [55, 56];
+      opts.lit = opts.lit || [35, 55];
+
+      const range = function (hash, min, max) {
+        const diff = max - min;
+        const x = ((hash % diff) + diff) % diff;
+        return x + min;
+      };
+      let hash = 0;
+      if (tag.length === 0) return hash;
+      for (let i = 0; i < tag.length; i += 1) {
+        // eslint-disable-next-line
+        hash = tag.charCodeAt(i) + ((hash << 5) - hash);
+        // eslint-disable-next-line
+        hash = hash & hash;
+      }
+
+      const h = range(hash, opts.hue[0], opts.hue[1]);
+      const s = range(hash, opts.sat[0], opts.sat[1]);
+      const l = range(hash, opts.lit[0], opts.lit[1]);
+
+      return `background-color: hsl(${h}, ${s}%, ${l}%)`;
     },
   },
   watch: {
