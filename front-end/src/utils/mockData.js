4eae8986e6d4675c7051271cffd5b29244db5426
jimmy b <mail@jimmyb.co.za>
2021-10-22 12:22:31 +0200

    Match view and edit styles - Used a mixin to apply the same styles across view and edit - Added my fliar to the elements so everything looks pretty - moved rules out of quill overrides that were not for quill

--- front-end/src/utils/mockData.js
+++ front-end/src/utils/mockData.js
@@ -1,11 +1,4 @@
-const mockData = `<h1>Title</h1>
-<strong>bold text</strong>
-<p>paragraph Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-Blanditiis dolore eum excepturi ipsa ipsam nisi officia officiis
-possimus recusandae veniam!</p>
-<p>The script which encapsulates the javascript vuey stuff:</p>
-<p><br></p>
-<pre className="ql-syntax" spellCheck="false">&lt;script&gt;
+const mockData = `<h1>Title</h1><p><strong>paragraph</strong> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis dolore eum excepturi ipsa ipsam nisi officia officiis possimus recusandae veniam!</p><p><br></p><p><strong><em><u>The script which encapsulates the javascript vuey stuff:</u></em></strong></p><pre class="ql-syntax" spellcheck="false">&lt;script&gt;
 import HelloWorld from './components/HelloWorld.vue'
 
 export default {
@@ -15,22 +8,6 @@ export default {
     }
   }
 &lt;/script&gt;
-  </pre>
-  <p><br></p>
-  <p>If you had a keen eye you would notice that the template has a non html tag named <strong>HelloWorld</strong>.
-  </p>
-  <p><br></p>
-  
-  <h2>Helpful Links</h2>
-  <p><br></p>
-  <ul>
-    <li>Some <a href="https://vueschool.io/lessons/getting-started-with-vuejs" rel="noopener noreferrer"
-                target="_blank">interactive lessons</a></li>
-    <li><a href="https://www.youtube.com/watch?v=FXpIoQ_rT_c" rel="noopener noreferrer" target="_blank">Free code
-      camp full tutorial</a></li>
-  </ul>
-  ${'<br>'.repeat(69)}
-  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tenetur, ullam.</p>
-`;
+</pre><p><br></p><p>top sentence</p><blockquote>If you had a keen eye you would notice that the template has a non html tag named <strong>HelloWorld</strong>.</blockquote><p>Another sentence</p><h2>Helpful Links</h2><ul><li>Some <a href="https://vueschool.io/lessons/getting-started-with-vuejs" rel="noopener noreferrer" target="_blank">interactive lessons</a></li><li><a href="https://www.youtube.com/watch?v=FXpIoQ_rT_c" rel="noopener noreferrer" target="_blank">Free code camp full tutorial</a></li></ul><p><br></p><p><br></p><p><br></p><p><br></p><p><br></p><p><br></p><p><br></p><p><br></p><p><br></p><p><br></p><p><br></p><p><br></p><p><br></p><p><br></p><p><br></p><p><br></p><p><br></p><p><br></p><p><br></p><p><br></p><p><br></p><p><br></p><p><br></p><p><br></p><p><br></p><p><br></p><p><br></p><p><br></p><p><br></p><p><br></p><p><br></p><p><br></p><p><br></p><p><br></p><p><br></p><p><br></p><p><br></p><p><br></p><p><br></p><p><br></p><p><br></p><p><br></p><p><br></p><p><br></p><p><br></p><p><br></p><p><br></p><p><br></p><p><br></p><p><br></p><p><br></p><p><br></p><p><br></p><p><br></p><p><br></p><p><br></p><p><br></p><p><br></p><p><br></p><p><br></p><p><br></p><p><br></p><p><br></p><p><br></p><p><br></p><p><br></p><p><br></p><p><br></p><p><br></p><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tenetur, ullam.</p>`;
 
 export default mockData;
