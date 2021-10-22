b0336b4161428d55d2968d760cc4e348ed4d281b
jimmy b <mail@jimmyb.co.za>
2021-10-22 09:57:52 +0200

    update mck server response to show better data for testing

---
+++ front-end/src/utils/mockData.js
@@ -0,0 +1,36 @@
+const mockData = `<h1>Title</h1>
+<strong>bold text</strong>
+<p>paragraph Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+Blanditiis dolore eum excepturi ipsa ipsam nisi officia officiis
+possimus recusandae veniam!</p>
+<p>The script which encapsulates the javascript vuey stuff:</p>
+<p><br></p>
+<pre className="ql-syntax" spellCheck="false">&lt;script&gt;
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+    name: 'App',
+    components: {
+      HelloWorld
+    }
+  }
+&lt;/script&gt;
+  </pre>
+  <p><br></p>
+  <p>If you had a keen eye you would notice that the template has a non html tag named <strong>HelloWorld</strong>.
+  </p>
+  <p><br></p>
+  
+  <h2>Helpful Links</h2>
+  <p><br></p>
+  <ul>
+    <li>Some <a href="https://vueschool.io/lessons/getting-started-with-vuejs" rel="noopener noreferrer"
+                target="_blank">interactive lessons</a></li>
+    <li><a href="https://www.youtube.com/watch?v=FXpIoQ_rT_c" rel="noopener noreferrer" target="_blank">Free code
+      camp full tutorial</a></li>
+  </ul>
+  ${'<br>'.repeat(69)}
+  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tenetur, ullam.</p>
+`;
+
+export default mockData;
