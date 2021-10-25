b2e5295c22d2137e849728807826af1a99cef365
Sebastian-Paterson-Jones <sebastian.boo.pj@gmail.com>
2021-10-25 14:48:24 +0200

    added toolbar elements for non markdown users

--- front-end/src/components/CreateEditDoc.vue
+++ front-end/src/components/CreateEditDoc.vue
@@ -137,6 +137,23 @@ export default {
       quillOptions: {
         theme: 'snow',
         modules: {
+          toolbar: [
+            ['bold', 'italic', 'underline', 'strike'],
+            ['blockquote', 'code-block'],
+            [{ header: 1 }, { header: 2 }],
+            [{ list: 'ordered' }, { list: 'bullet' }],
+            [{ script: 'sub' }, { script: 'super' }],
+            [{ indent: '-1' }, { indent: '+1' }],
+            [{ direction: 'rtl' }],
+
+            [{ size: ['small', false, 'large', 'huge'] }],
+            [{ header: [1, 2, 3, 4, 5, 6, false] }],
+            ['link', 'image', 'video', 'formula'],
+            [{ color: [] }, { background: [] }],
+            [{ font: [] }],
+            [{ align: [] }],
+            ['clean'],
+          ],
           markdownShortcuts: {},
         },
       },
