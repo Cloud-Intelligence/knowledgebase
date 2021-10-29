d6af45e741a9f13ed7795e386ea3390734972d4e
jimmy b <mail@jimmyb.co.za>
2021-10-29 09:50:49 +0200

    revert updated test timing to remove flakyness on ci server, includes updated scrots

--- front-end/cypress/integration/create.spec.js
+++ front-end/cypress/integration/create.spec.js
@@ -1,20 +1,25 @@
 describe('create', () => {
     it('matches image baseline', () => {
         cy.visit('/create');
+        cy.wait(1000);
         cy.get('#create').toMatchImageSnapshot();
     });
     it('has topics dropdown', () => {
         cy.visit('/create');
+        cy.wait(500);
         cy.get('.trigger').first().click();
         cy.get('.trigger').first().toMatchImageSnapshot();
         cy.get('.dropdown-content').first().toMatchImageSnapshot();
     });
     it('validates user input', () => {
         cy.visit('/create');
+        cy.wait(500);
         cy.get('.submit .button').click();
         cy.get('#create').toMatchImageSnapshot();
     });
     it('corrects validation on field input', () => {
+        cy.get('#title').click();
+        cy.wait(250);
         cy.get('#title').type('Test');
         cy.get('.topic-input.trigger').type('New topic');
         cy.get('.tag-input.trigger').click();
@@ -26,11 +31,13 @@ describe('create', () => {
     });
     it('allows adding new topics', () => {
         cy.visit('/create');
+        cy.wait(500);
         cy.get('.topic-input.trigger').type('New topic');
         cy.get('.topic-input.trigger').toMatchImageSnapshot();
     });
     it('allows adding new tags', () => {
         cy.visit('/create');
+        cy.wait(500);
         cy.get('.tag-input.trigger').click();
         cy.get('.dropdown-item input').last().type('New tag');
         cy.get('.dropdown-item button').last().click();
@@ -38,6 +45,7 @@ describe('create', () => {
     });
     it('redirects after the document has been posted', () => {
        cy.visit('/create');
+       cy.wait(500);
        cy.get('#title').type('Test title');
        cy.get('.topic-input.trigger').type('New topic name');
        cy.get('.tag-input.trigger').click();
@@ -45,6 +53,7 @@ describe('create', () => {
        cy.get('.dropdown-item button').last().click();
        cy.get('.ql-editor').type('test');
        cy.get('.submit .button').click();
-       cy.get('.body').toMatchImageSnapshot();
+      cy.wait(500);
+      cy.get('.body').toMatchImageSnapshot();
     });
 });
