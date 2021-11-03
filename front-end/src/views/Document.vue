c01834f7454b84b8bab6bf3a14179de735356b46
Sebastian-Paterson-Jones <sebastian.boo.pj@gmail.com>
2021-11-03 10:47:41 +0200

    refactored getBackgroundColor method, no repeated code now

--- front-end/src/views/Document.vue
+++ front-end/src/views/Document.vue
@@ -61,6 +61,8 @@ import { UilTrashAlt, UilEdit, UilStar } from '@iconscout/vue-unicons';
 import Spinner from 'vue-simple-spinner';
 import { deleteDocument, getDocument } from '../api/documents';
 
+import getBackgroundColor from '../utils/dynamic_colours';
+
 export default {
   name: 'Document',
   components: {
@@ -113,32 +115,7 @@ export default {
     editDoc() {
       this.$router.push({ path: `/edit/${this.id}` });
     },
-    getBackgroundColor(tag) {
-      const opts = {};
-      opts.hue = opts.hue || [0, 360];
-      opts.sat = opts.sat || [55, 56];
-      opts.lit = opts.lit || [35, 55];
-
-      const range = function (hash, min, max) {
-        const diff = max - min;
-        const x = ((hash % diff) + diff) % diff;
-        return x + min;
-      };
-      let hash = 0;
-      if (tag.length === 0) return hash;
-      for (let i = 0; i < tag.length; i += 1) {
-        // eslint-disable-next-line
-        hash = tag.charCodeAt(i) + ((hash << 5) - hash);
-        // eslint-disable-next-line
-        hash = hash & hash;
-      }
-
-      const h = range(hash, opts.hue[0], opts.hue[1]);
-      const s = range(hash, opts.sat[0], opts.sat[1]);
-      const l = range(hash, opts.lit[0], opts.lit[1]);
-
-      return `background-color: hsl(${h}, ${s}%, ${l}%)`;
-    },
+    getBackgroundColor,
     highlightCode() {
       const codeBlocks = this.$refs.content.querySelectorAll('pre');
       console.log(codeBlocks);
