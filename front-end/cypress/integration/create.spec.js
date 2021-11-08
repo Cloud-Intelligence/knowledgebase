3a80a5a31643a56b78a270e11544fcb1a527e1c8
Sebastian-Paterson-Jones <sebastian.boo.pj@gmail.com>
2021-11-08 16:14:34 +0200

    test updated due to change in structure of pages

--- front-end/cypress/integration/create.spec.js
+++ front-end/cypress/integration/create.spec.js
@@ -1,59 +1,34 @@
 describe('create', () => {
-    it('matches image baseline', () => {
-        cy.visit('/create');
-        cy.wait(1000);
-        cy.get('#create').toMatchImageSnapshot();
-    });
-    it('has topics dropdown', () => {
-        cy.visit('/create');
-        cy.wait(500);
-        cy.get('.trigger').first().click();
-        cy.get('.trigger').first().toMatchImageSnapshot();
-        cy.get('.dropdown-content').first().toMatchImageSnapshot();
-    });
-    it('validates user input', () => {
-        cy.visit('/create');
-        cy.wait(500);
-        cy.get('.submit .button').click();
-        cy.get('#create').toMatchImageSnapshot();
-    });
-    it('corrects validation on field input', () => {
-        cy.get('#title').click();
-        cy.wait(250);
-        cy.get('#title').type('Test');
-        cy.get('.topic-input.trigger').type('New topic');
-        cy.get('.tag-input.trigger').click();
-        cy.get('.dropdown-item input').last().type('New tag');
-        cy.get('.dropdown-item button').last().click();
-
-        cy.get('.ql-editor').type('test');
-        cy.get('#create').toMatchImageSnapshot();
-    });
-    it('allows adding new topics', () => {
-        cy.visit('/create');
-        cy.wait(500);
-        cy.get('.topic-input.trigger').type('New topic');
-        cy.get('.topic-input.trigger').toMatchImageSnapshot();
-    });
-    it('allows adding new tags', () => {
-        cy.visit('/create');
-        cy.wait(500);
-        cy.get('.tag-input.trigger').click();
-        cy.get('.dropdown-item input').last().type('New tag');
-        cy.get('.dropdown-item button').last().click();
-        cy.get('.tag-input.trigger').toMatchImageSnapshot();
-    });
-    it('redirects after the document has been posted', () => {
-       cy.visit('/create');
-       cy.wait(500);
-       cy.get('#title').type('Test title');
-       cy.get('.topic-input.trigger').type('New topic name');
-       cy.get('.tag-input.trigger').click();
-       cy.get('.dropdown-item input').last().type('New tag');
-       cy.get('.dropdown-item button').last().click();
-       cy.get('.ql-editor').type('test');
-       cy.get('.submit .button').click();
-      cy.wait(500);
-      cy.get('.body').toMatchImageSnapshot();
-    });
+  it('matches image baseline', () => {
+    cy.visit('/create');
+    cy.wait(1000);
+    cy.get('.body').toMatchImageSnapshot();
+  });
+  it('has topics dropdown', () => {
+    cy.visit('/create');
+    cy.wait(500);
+    cy.get('.field .input').first().click();
+    cy.get('.form').first().toMatchImageSnapshot();
+  });
+  it('validates user input', () => {
+    cy.visit('/create');
+    cy.wait(500);
+    cy.get('.submit .button').click();
+    cy.get('.body').toMatchImageSnapshot();
+  });
+  it('allows adding new topics', () => {
+    cy.visit('/create');
+    cy.wait(500);
+    cy.get('.field .input').first().type('New topic');
+    cy.get('.field .input').first().toMatchImageSnapshot();
+  });
+  it('redirects after the document has been posted', () => {
+    cy.visit('/create');
+    cy.wait(500);
+    cy.get('.field .input').first().type('New topic');
+    cy.get('.field .input').first().type('New topic');
+    cy.get('.submit .button').click();
+    cy.wait(500);
+    cy.get('.body').toMatchImageSnapshot();
+  });
 });
