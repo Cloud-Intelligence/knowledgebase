6488ebded88dafff243c943e7004be32f4ae17ec
sebastian paterson-jones <sebastian.boo.pj@gmail.com>
2021-11-08 18:01:00 +0200

    modified scrots becuase of changes to the create page and edit page (#80)
    * modified scrots becuase of changes to the create page and edit page
    
    * test updated due to change in structure of pages
    
    * scrots updated for those tests that were updated because they previosly failed
    
    * test updated because failing on local due to latency

--- front-end/cypress/integration/edit.spec.js
+++ front-end/cypress/integration/edit.spec.js
@@ -1,24 +1,24 @@
 describe('edit', () => {
-    it('routes and matches image baseline of edit doc', () => {
-        cy.visit('/');
-        cy.get('.topic button').first().click();
-        cy.get('#12').first().click();
-        cy.get('.title-bar .icons .edit').first().click();
-        cy.wait(1000); // for api response
-        cy.get('#edit').toMatchImageSnapshot();
-    });
-    it('From fields are populated', () => {
-        cy.get('#edit').toMatchImageSnapshot();
-    });
-    it('has topics in dropdown', () => {
-        cy.get('.topic-input.trigger').first().click();
-        cy.get('.dropdown-content').first().toMatchImageSnapshot();
-        cy.get('.topic-input.trigger').first().click();
-    });
-    it('redirects after the document has been updated', () => {
-        cy.get('.topic-input.trigger').type('blah');
-        cy.get('.submit .button').click();
-        cy.wait(1000)
-        cy.get('.body').toMatchImageSnapshot();
-    });
+  it('routes and matches image baseline of edit doc', () => {
+    cy.visit('/');
+    cy.get('.topic button').first().click();
+    cy.get('#12').first().click();
+    cy.get('.title-bar .icons .edit').first().click();
+    cy.wait(1000); // for api response
+    cy.get('.body').toMatchImageSnapshot();
+  });
+  it('From fields are populated', () => {
+    cy.get('.body').toMatchImageSnapshot();
+  });
+  it('has topics in dropdown', () => {
+    cy.get('.topic-input.trigger').first().click();
+    cy.get('.dropdown-content').first().toMatchImageSnapshot();
+    cy.get('.topic-input.trigger').first().click();
+  });
+  it('redirects after the document has been updated', () => {
+    cy.get('.topic-input.trigger').type('blah');
+    cy.get('.submit .button').click();
+    cy.wait(1000);
+    cy.get('.body').toMatchImageSnapshot();
+  });
 });
